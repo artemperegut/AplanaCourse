4fd8a359e5f42879b78d6bc768a12245a6904c2f
Artem <artem.peregut@gmail.com>
2017-05-13 12:24:57 +0300

    Убрал лишние импорты

--- src/TaskEnvironment.java
+++ src/TaskEnvironment.java
@@ -1,9 +1,5 @@
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class TaskEnvironment {
     public static void main(String[] args) {
